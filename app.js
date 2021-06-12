a4f263fbeaff758feea15ab6424d332a2c068592
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2021-06-11 20:36:00 -0400

    second commit

--- app.js
+++ app.js
@@ -39,3 +39,48 @@ const reviews = [
 ];
 
 // select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const randomBtn = document.querySelector('.random-btn')
+
+// set starting item
+let currentItem = 0;
+
+//load inital item
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+//show person based on item
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+
+nextBtn.addEventListener('click', () => {
+  currentItem++
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+})
+
+prevBtn.addEventListener('click', () => {
+  currentItem--
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+})
+
+// show
